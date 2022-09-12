ac2991e104b332c63ebce476ef1b3bb5b0fbed02
Ian <iangaunt999@gmail.com>
2022-09-12 13:33:17 -0700

    Reformatted some important files

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -9,35 +9,37 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"unused", "PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+	@SuppressWarnings({"unused", "PMD.UnusedPrivateField", "PMD.SingularField"})
+  	private final ExampleSubsystem m_subsystem;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
-  }
+	/**
+	 * Creates a new ExampleCommand.
+	 *
+	 * @param subsystem The subsystem used by this command.
+	 */
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+	public ExampleCommand(ExampleSubsystem subsystem) {
+		m_subsystem = subsystem;
+		
+		// Use addRequirements() here to declare subsystem dependencies.
+		addRequirements(subsystem);
+	}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {}
+
+	// Returns true when the command should end.
+	@Override
+		public boolean isFinished() {
+		return false;
+	}
 }
